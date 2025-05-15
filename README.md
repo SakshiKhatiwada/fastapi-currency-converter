c89733291696e541baac85496160f346815c5c5a
Sakshi Khatiwada <sakshikhatiwada37@gmail.com>
2025-05-15 13:02:43 +0545

    feat: precommit hook setup

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # fastapi-currency-converter
 Currency Converter App Created with FastAPI under Fuse Machines AI Fellowship Assignment I
+
+
+pre-commit install
+pre-commit run --all-files
--- app/config.py
+++ app/config.py
@@ -4,8 +4,8 @@ from pydantic_settings import BaseSettings, SettingsConfigDict
 class Settings(BaseSettings):
     site_title: str
     site_version: str
-    
+
     model_config = SettingsConfigDict(env_file=".env")
-    
+
 
 settings = Settings()
