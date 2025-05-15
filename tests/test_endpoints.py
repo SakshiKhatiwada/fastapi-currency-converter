a2af56320ac166fbc9999650fda1a45dba58d19a
Sakshi Khatiwada <sakshikhatiwada37@gmail.com>
2025-05-15 16:03:04 +0545

    test code for converter api

--- tests/test_endpoints.py
+++ tests/test_endpoints.py
@@ -1,5 +1,6 @@
 from fastapi.testclient import TestClient
 from app.main import app
+from app.utils import currency_converter
 
 client = TestClient(app)
 
@@ -8,3 +9,13 @@ def test_site_check():
     response = client.get("/check")
     assert response.status_code == 200
     assert response.json()["status"] == "ok"
+
+
+def test_converter_api():
+    payload = {"from_curr": "USD", "to_curr": "GBP", "amount": 100}
+
+    response = client.post("/api/convert", json=payload)
+    assert response.status_code == 200
+
+    converted_output = currency_converter(payload)
+    assert response.json() == converted_output
